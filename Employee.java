099e5b19a0820c9915359d6777c8892d33d799d2
txq6163 <48515632+txq6163@users.noreply.github.com>
2019-03-14 15:27:42 +0900

    HR管理システム
    HR管理システム　追加

---
+++ Employee.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Sheyuanqingbao;
+
+/**
+ *
+ * @author user
+ */
+public class Employee {
+
+    //社員ID
+    private String eId;
+    //社員名称
+    private String eName;
+    //性別
+    private String eSex;
+    //職位
+    private String ePos;
+    // 入社日
+    private String eEntrydate;
+
+    /**
+     * @return the eId
+     */
+    public String geteId() {
+        return eId;
+    }
+
+    /**
+     * @param eId the eId to set
+     */
+    public void seteId(String eId) {
+        this.eId = eId;
+    }
+
+    /**
+     * @return the eName
+     */
+    public String geteName() {
+        return eName;
+    }
+
+    /**
+     * @param eName the eName to set
+     */
+    public void seteName(String eName) {
+        this.eName = eName;
+    }
+
+    /**
+     * @return the eSex
+     */
+    public String geteSex() {
+        return eSex;
+    }
+
+    /**
+     * @param eSex the eSex to set
+     */
+    public void seteSex(String eSex) {
+        this.eSex = eSex;
+    }
+
+    /**
+     * @return the ePos
+     */
+    public String getePos() {
+        return ePos;
+    }
+
+    /**
+     * @param ePos the ePos to set
+     */
+    public void setePos(String ePos) {
+        this.ePos = ePos;
+    }
+
+    /**
+     * @return the eEntrydate
+     */
+    public String geteEntrydate() {
+        return eEntrydate;
+    }
+
+    /**
+     * @param eEntrydate the eEntrydate to set
+     */
+    public void seteEntrydate(String eEntrydate) {
+        this.eEntrydate = eEntrydate;
+    }
+
+}
---
+++ EmployeeService.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Sheyuanqingbao;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+/**
+ *
+ * @author user
+ */
+public class EmployeeService {
+
+    /**
+     * 社員情報一括登録
+     *
+     * @param csvFile
+     * @return
+     */
+    int importEmployeeDataFromCSV(String csvFile) {
+
+        return 0;
+    }
+
+    /**
+     * 社員情報個別登録
+     */
+    boolean createEmployeeData(String eName, String eSex, String ePos) {
+
+        return true;
+    }
+}
+
